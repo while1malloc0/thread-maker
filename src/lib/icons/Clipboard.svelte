28f9474399ae22bf3153ac4d15aa5377f8e2ddd3
John Turner <johnturner@me.com>
2022-02-28 05:30:07 +0000

    fix(style): use pointer cursor for clipboard

--- src/lib/icons/Clipboard.svelte
+++ src/lib/icons/Clipboard.svelte
@@ -8,7 +8,7 @@
   };
 </script>
 
-<div on:click={onClick} class="transition-all">
+<div on:click={onClick} class="transition-all cursor-pointer">
   {#if clicked}
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path
