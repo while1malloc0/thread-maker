f6c380c727cf6d25332eac98cba8f899cb378f97
John Turner <johnturner@me.com>
2022-02-28 04:09:41 +0000

    refactor(style): clean up some styles and add some visual flair

---
+++ src/lib/icons/ArrowRight.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let classes;
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" class={classes} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+</svg>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import Clipboard from "$lib/icons/Clipboard.svelte";
+  import ArrowRight from "$lib/icons/ArrowRight.svelte";
+
   const clip = (content: string) => {
     navigator.clipboard.writeText(content);
   };
 
   let input;
   let state: { tweets?: string[]; errors?: any } = { tweets: [], errors: {} };
-  $: textAreaBorder = state.errors?.tooLong ? "border-red-500" : "border-gray-500";
+  $: textAreaBorder = state.errors?.banner ? "border-red-500" : "border-gray-500";
 
   const splitThread = (input: string) => {
     if (!input || input.length <= 1) {
@@ -18,10 +20,8 @@
     for (let sentence of sentences) {
       // case 1: sentence is too long
       if (sentence.length > 280) {
-        // TODO do better error handling
         return {
           errors: {
-            tooLong: sentence,
             banner: `Error: the sentence ${sentence.slice(0, 30)}... is too long to fit into a Tweet.`,
           },
           tweets: state.tweets,
@@ -46,18 +46,20 @@
     <div class="bg-red-500 text-white text-center p-2">{state.errors.banner}</div>
   {/if}
 </div>
-<div class="p-5 grid grid-flow-col grid-cols-3">
+<h1 class="px-5 py-2 text-3xl">Thread maker</h1>
+<div class="p-5 grid grid-flow-col grid-cols-4">
   <div class="col-span-2 p-2 border-2 {textAreaBorder} rounded-md">
     <textarea
       on:keyup={() => (state = splitThread(input))}
       bind:value={input}
       class="w-full h-screen"
-      placeholder="Start typing your thread here..."
+      placeholder="Long text goes in here..."
     />
   </div>
-  <div class="col-span-1 p-2 grid grid-flow-row">
+  <div class="grid items-center justify-center"><ArrowRight classes="w-40 h-40" /></div>
+  <div class="col-span-1 p-2 grid grid-flow-row border-2 border-gray-500 rounded-md">
     {#if state.tweets.length < 1}
-      <div class="shadow-md rounded-md"><p class="p-2">And it'll appear split up here.</p></div>
+      <div class="shadow-md rounded-md"><p class="p-2">...and comes out as a thread here</p></div>
     {/if}
     {#each state.tweets as tweet}
       <div class="shadow-md rounded-md max-h-content border-1 border-gray-100">
